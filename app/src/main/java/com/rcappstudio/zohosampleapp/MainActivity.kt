09a205568cc13bdfeff21ff64144d26bc41fda14
HARI HARAN R C <81560319+HariHaran007-hub@users.noreply.github.com>
2022-09-10 21:59:25 +0530

    ssss

---
+++ app/src/main/java/com/rcappstudio/zohosampleapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.rcappstudio.zohosampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.zoho.catalyst.setup.ZCatalystApp
+import com.zoho.catalyst.setup.ZCatalystSDKConfigs
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val a= ZCatalystApp.init(applicationContext)
+        Log.d("TAG", "onCreate:${a.getDataStoreInstance()} ")
+
+
+
+    }
+}
